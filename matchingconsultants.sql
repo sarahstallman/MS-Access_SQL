d4412ac3531110970117d446db6339eb5028e17f
sarahstallman <142286115+sarahstallman@users.noreply.github.com>
2023-09-15 18:59:59 -0700

    Update matchingconsultants.sql

--- matchingconsultants.sql
+++ matchingconsultants.sql
@@ -2,3 +2,11 @@ SELECT [consultants p&l].source, [consultants p&l].date, [consultants p&l].capti
 INTO CONSULTANTS
 FROM [consultants p&l] LEFT JOIN [rollup master list] ON [consultants p&l].source = [rollup master list].source
 WHERE [rollup master list].company = 'consultants';
+
+#This SQL code selects specific columns from two tables, "[consultants p&l]" and "[rollup master list]," and performs a LEFT JOIN on the "source" column between them. It creates a new table called "CONSULTANTS."
+
+#The columns selected include "source," "date," "captive," "[pension/life]," "health," and "[general lines]" from the "[consultants p&l]" table. Additionally, it selects "[captive rollup]," "[pension rollup]," "[health rollup]," "[general rollup]," "division," "type," and "category" from the "[rollup master list]" table.
+
+#The LEFT JOIN operation combines the data from both tables based on matching "source" values. It retrieves rows from the "[consultants p&l]" table even if there is no corresponding "source" in the "[rollup master list]" table.
+
+#The result includes records where the "company" column in the "[rollup master list]" table has the value 'consultants.' The data from both tables are then inserted into the new "CONSULTANTS" table.
