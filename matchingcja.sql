f9eaebdf4c37f8bb9f4d09d0f0497ee87c2bb849
sarahstallman <142286115+sarahstallman@users.noreply.github.com>
2023-09-15 18:59:28 -0700

    Update matchingcja.sql

--- matchingcja.sql
+++ matchingcja.sql
@@ -2,3 +2,12 @@ SELECT [cja p&l].source, [cja p&l].date, [cja p&l].corporate, [cja p&l].[life/re
 INTO CJA
 FROM [cja p&l] LEFT JOIN [rollup master list] ON [cja p&l].source = [rollup master list].source
 WHERE [rollup master list].company = 'cja';
+
+#This SQL code selects specific columns from two tables, "[cja p&l]" and "[rollup master list]," and performs a LEFT JOIN on the "source" column between them. It creates a new table called "CJA."
+
+#The columns selected include "source," "date," "corporate," "[life/retirement division]," "health," and "total" from the "[cja p&l]" table. Additionally, it selects "[pension rollup]," "[health rollup]," "division," "type," and "category" from the "[rollup master list]" table.
+
+#The LEFT JOIN operation combines the data from both tables based on matching "source" values. It retrieves rows from the "[cja p&l]" table even if there is no corresponding "source" in the "[rollup master list]" table.
+
+#The result includes records where the "company" column in the "[rollup master list]" table has the value 'cja.' The data from both tables are then inserted into the new "CJA" table.
+
