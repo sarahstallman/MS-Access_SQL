4850b93256ba84039e7451ca454b7781be632185
sarahstallman <142286115+sarahstallman@users.noreply.github.com>
2023-09-15 18:54:49 -0700

    Update healthfinal.sql

--- healthfinal.sql
+++ healthfinal.sql
@@ -34,3 +34,12 @@ FROM (SELECT
         FROM CONSULTANTSHEALTH
     )  AS CombinedTables
 GROUP BY CombinedTables.Type, CombinedTables.Category, CombinedTables.[health rollup], CombinedTables.[Date];
+
+#This SQL code creates a new table called "[Health Division]" by combining data from three different tables: "[P&CHEALTH]," "CJAHEALTH," and "CONSULTANTSHEALTH." The code performs three separate SELECT queries using UNION ALL to combine data from each source table.
+
+#Each SELECT query within the UNION ALL includes columns for "Type," "Category," "[health rollup]," and "[Date]," along with columns for summing values from each source table, named "PC_SUM," "CJA_SUM," and "Consultants_SUM."
+
+#The main query then groups the data from the UNION ALL results based on "Type," "Category," "[health rollup]," and "[Date]." It calculates the sum of "PC_SUM," "CJA_SUM," and "Consultants_SUM" for each group using the SUM function.
+
+#The result is a new table "[Health Division]" containing grouped data from the three source tables, where each row represents a unique combination of "Type," "Category," "[health rollup]," and "[Date]," along with the corresponding sums from each source table. The calculated total sum of "PC_SUM," "CJA_SUM," and "Consultants_SUM" for each group is also included in the new table.
+
